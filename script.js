a45cce94c843762f82cc98d7079e7111aeeb99f9
janak rathod <61515313+janak06@users.noreply.github.com>
2021-12-11 14:11:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 function Cart() {
   var [products, setProducts] = React.useState([
-  { name: "monitor", qty: 0, price: 6000 },
-  { name: "mouse", qty: 0, price: 2500 },
-  { name: "keyboard", qty: 0, price: 1800 },
-  { name: "speaker", qty: 0, price: 4500 },
-  { name: "headphones", qty: 0, price: 2300 },
-  { name: "laptop", qty: 0, price: 55000 }]);
-
+    { name: "monitor", qty: 0, price: 6000 },
+    { name: "mouse", qty: 0, price: 2500 },
+    { name: "keyboard", qty: 0, price: 1800 },
+    { name: "speaker", qty: 0, price: 4500 },
+    { name: "headphones", qty: 0, price: 2300 },
+    { name: "laptop", qty: 0, price: 55000 }
+  ]);
   var [totalQty, setTotalQty] = React.useState(0);
   var [totalPrice, setTotalPrice] = React.useState(0);
-
-  React.useEffect(() => {
-    var totalQtyfromArray = products.map(a => a.qty);
-    var totalPrice = products.map(a => a.qty * a.price);
+  
+  React.useEffect(() => {  
+    var totalQtyfromArray = products.map((a) => a.qty);
+    var totalPrice = products.map((a) => a.qty * a.price);
     setTotalPrice(totalPrice.reduce((r, t) => r + t));
     setTotalQty(totalQtyfromArray.reduce((r, t) => r + t));
-  }, [products]);
+   }, [products]);
 
   const handleClick = (index, qty) => {
     var chnageqty = [...products];
     chnageqty[index] = { ...chnageqty[index], qty: qty };
     setProducts(chnageqty);
   };
-
-  return /*#__PURE__*/(
-    React.createElement("div", null, /*#__PURE__*/
-    React.createElement("div", null, /*#__PURE__*/
-    React.createElement("h1", null, "Cart")),
-
-    products === null || products === void 0 ? void 0 : products.map((item, i) => /*#__PURE__*/
-    React.createElement("div", { key: i }, /*#__PURE__*/
-    React.createElement("h2", null,
-    item.name, " =>Price:", item.price, " ", "Qty:", item.qty), /*#__PURE__*/
-
-    React.createElement("div", null, /*#__PURE__*/
-    React.createElement("button", { onClick: () => handleClick(i, item.qty + 1) }, "+"), "\xA0\xA0\xA0", /*#__PURE__*/
-    React.createElement("button", { onClick: () => item.qty != 0 ? handleClick(i, item.qty - 1) : null }, "-")))), /*#__PURE__*/
-
-
-
-    React.createElement("div", null, /*#__PURE__*/
-    React.createElement("h2", null, "Total item:", totalQty), /*#__PURE__*/
-    React.createElement("h2", null, "Total Amount:", totalPrice))));
-
-
-
+  
+  return (
+    <div>
+      <div>
+        <h1>Cart</h1>
+      </div>
+      {products?.map((item, i) => (
+        <div key={i}>
+          <h2>
+            {item.name} =>Price:{item.price}{" "}Qty:{item.qty}
+          </h2>
+          <div>
+            <button onClick={() => handleClick(i, item.qty+1)}>+</button>&nbsp;&nbsp;&nbsp;
+            <button onClick={() => item.qty!=0?handleClick(i, item.qty-1):null}>-</button>
+          </div>
+        </div>
+      ))}
+      <div>
+        <h2>Total item:{totalQty}</h2>
+        <h2>Total Amount:{totalPrice}</h2>
+      </div>
+    </div>
+  );
 }
-ReactDOM.render( /*#__PURE__*/React.createElement(Cart, null), document.getElementById("root"));
+ReactDOM.render(<Cart />, document.getElementById("root"));
